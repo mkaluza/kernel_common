79190923a8f2a120401cccd5258ab512315c4fa6
Marcin Kaluza <marcin.kaluza@trioptimum.com>
2015-01-09 16:27:00 +0000

    fs_partitions

--- fs/partitions/check.c
+++ fs/partitions/check.c
@@ -27,6 +27,7 @@
 #include "acorn.h"
 #include "amiga.h"
 #include "atari.h"
+#include "blkdev_parts.h"
 #include "ldm.h"
 #include "mac.h"
 #include "msdos.h"
@@ -38,6 +39,7 @@
 #include "efi.h"
 #include "karma.h"
 #include "sysv68.h"
+#include "rpmb.h"
 
 #ifdef CONFIG_BLK_DEV_MD
 extern void md_autodetect_dev(dev_t dev);
@@ -50,6 +52,17 @@ static int (*check_part[])(struct parsed_partitions *) = {
 	 * Probe partition formats with tables at disk address 0
 	 * that also have an ADFS boot block at 0xdc0.
 	 */
+#ifdef CONFIG_BLKDEV_PARTITION
+	blkdev_partition,
+#endif
+#ifdef CONFIG_RPMB_PARTITION
+	/*
+	 * Must be before any formats which have a partition table so that no
+	 * attempt is made to access replay protected memory block (RPMB)
+	 * partitions.
+	 */
+	rpmb_partition,
+#endif
 #ifdef CONFIG_ACORN_PARTITION_ICS
 	adfspart_check_ICS,
 #endif
