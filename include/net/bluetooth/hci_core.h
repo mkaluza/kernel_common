2c7b8978c1e7dcf71d6c66d48e5dcd0ce02cc4fc
Marcin Kaluza <marcin.kaluza@trioptimum.com>
2015-01-11 15:05:36 +0000

    merge: 6c77c8c -- include/net/bluetooth/hci_core.h

--- include/net/bluetooth/hci_core.h
+++ include/net/bluetooth/hci_core.h
@@ -582,7 +582,7 @@ int hci_add_remote_oob_data(struct hci_dev *hdev, bdaddr_t *bdaddr, u8 *hash,
 int hci_remove_remote_oob_data(struct hci_dev *hdev, bdaddr_t *bdaddr);
 
 #define ADV_CLEAR_TIMEOUT (3*60*HZ) /* Three minutes */
-int hci_adv_entries_clear(struct hci_dev *hdev);
+nt hci_adv_entries_clear(struct hci_dev *hdev);
 struct adv_entry *hci_find_adv_entry(struct hci_dev *hdev, bdaddr_t *bdaddr);
 int hci_add_adv_entry(struct hci_dev *hdev,
 					struct hci_ev_le_advertising_info *ev);
