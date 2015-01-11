1daac9740313564cbe7257aff9c3ca4a73d3ab73
Marcin Kaluza <marcin.kaluza@trioptimum.com>
2015-01-11 09:28:11 +0000

    merge: 7c558b7..upgr -- fs/jbd2/transaction.c

--- fs/jbd2/transaction.c
+++ fs/jbd2/transaction.c
@@ -178,8 +178,21 @@ repeat:
 		if (!new_transaction)
 			goto alloc_transaction;
 		write_lock(&journal->j_state_lock);
+<<<<<<< ours
+
+		/* add to bug fix code, j.gap.lee@samsung.com, 2012.04.23 */
+		/* barrier check */
+		if (journal->j_barrier_count) {
+			write_unlock(&journal->j_state_lock);
+			goto repeat;
+		}
+		/* end */
+
+		if (!journal->j_running_transaction) {
+=======
 		if (!journal->j_running_transaction &&
 		    !journal->j_barrier_count) {
+>>>>>>> theirs
 			jbd2_get_transaction(journal, new_transaction);
 			new_transaction = NULL;
 		}
@@ -1675,8 +1688,20 @@ int jbd2_journal_try_to_free_buffers(journal_t *journal,
 		__journal_try_to_free_buffer(journal, bh);
 		jbd2_journal_put_journal_head(jh);
 		jbd_unlock_bh_state(bh);
+#ifndef CONFIG_DMA_CMA
 		if (buffer_jbd(bh))
 			goto busy;
+#else
+		if (buffer_jbd(bh)) {
+			/*
+			 * Workaround: In case of CMA page, just commit journal.
+			 */
+			if (is_cma_pageblock(page))
+				jbd2_journal_force_commit(journal);
+			else
+				goto busy;
+		}
+#endif
 	} while ((bh = bh->b_this_page) != head);
 
 	ret = try_to_free_buffers(page);
