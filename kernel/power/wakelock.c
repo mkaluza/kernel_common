89e79b899547ff41e45d3f97d503f4d02db45324
Marcin Kaluza <marcin.kaluza@trioptimum.com>
2015-01-11 11:02:12 +0000

    merge: 7013f49..upgr -- kernel/power/wakelock.c

--- kernel/power/wakelock.c
+++ kernel/power/wakelock.c
@@ -24,6 +24,13 @@
 #endif
 #include "power.h"
 
+
+#ifdef CONFIG_SVNET_WHITELIST
+#include <linux/delay.h>
+#include "portlist.h"
+extern int jig_smd; /* [Modem] 1 == local mode, 0 == normal mode */
+#endif /* CONFIG_SVNET_WHITELIST */
+
 enum {
 	DEBUG_EXIT_SUSPEND = 1U << 0,
 	DEBUG_WAKEUP = 1U << 1,
@@ -270,7 +277,7 @@ static void suspend_backoff(void)
 
 static void suspend(struct work_struct *work)
 {
-	int ret;
+	int ret, ret1;
 	int entry_event_num;
 	struct timespec ts_entry, ts_exit;
 
@@ -280,12 +287,50 @@ static void suspend(struct work_struct *work)
 		return;
 	}
 
+
+#ifdef CONFIG_SVNET_WHITELIST
+	pr_info("[%s] jig_smd: %d\n", __func__, jig_smd);
+	if (jig_smd == 0) { /* modem == normal mode */
+		ret = process_whilte_list();
+		pr_info("[%s]-process_whilte_list(): %d\n", __func__, ret);
+
+		if (unlikely(ret != 0)) {
+			pr_info("fail to send whitelist\n");
+			return;
+		} else {
+			ret1 = tx_ap_state(AP_STATE_SLEEP);
+			pr_info("tx_ap_state(SLEEP): %d\n", ret1);
+			if (unlikely(ret1 != 0)) {
+				pr_info("fail to send SLEEP IPC\n");
+				return;
+			}
+			if (has_wake_lock(WAKE_LOCK_SUSPEND)) {
+				if (debug_mask & DEBUG_SUSPEND)
+					pr_info("suspend: abort suspend after processing white list\n");
+				return;
+			}
+		}
+	}
+#endif
+
 	entry_event_num = current_event_num;
 	sys_sync();
 	if (debug_mask & DEBUG_SUSPEND)
 		pr_info("suspend: enter suspend\n");
 	getnstimeofday(&ts_entry);
 	ret = pm_suspend(requested_suspend_state);
+
+#ifdef CONFIG_SVNET_WHITELIST
+	if (jig_smd == 0) { /* modem == normal mode */
+		ret1 = tx_ap_state(AP_STATE_WAKEUP);
+		pr_info("tx_ap_state(WAKEUP): %d\n", ret1);
+		if (unlikely(ret1 != 0)) {
+			pr_info("fail to send WAKEUP IPC\n");
+			return;
+		}
+	}
+#endif
+
 	getnstimeofday(&ts_exit);
 
 	if (debug_mask & DEBUG_EXIT_SUSPEND) {
