2662e5534511a38908312b7f0574a40bed0f5f69
Marcin Kaluza <marcin.kaluza@trioptimum.com>
2015-01-11 12:53:35 +0000

    merge: 76b3990 -- kernel/printk.c

--- kernel/printk.c
+++ kernel/printk.c
@@ -58,7 +58,7 @@ void asmlinkage __attribute__((weak)) early_printk(const char *fmt, ...)
 
 #define __LOG_BUF_LEN	(1 << CONFIG_LOG_BUF_SHIFT)
 
-#ifdef        CONFIG_PRINTK_LL
+#ifdef        CONFIG_DEBUG_LL
 extern void printascii(char *);
 #endif
 
@@ -987,7 +987,7 @@ asmlinkage int vprintk(const char *fmt, va_list args)
 	printed_len += vscnprintf(printk_buf + printed_len,
 				  sizeof(printk_buf) - printed_len, fmt, args);
 
-#ifdef	CONFIG_PRINTK_LL
+#ifdef	CONFIG_DEBUG_LL
 	printascii(printk_buf);
 #endif
 
