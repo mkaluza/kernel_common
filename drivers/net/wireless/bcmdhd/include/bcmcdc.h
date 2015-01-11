9bde3c7e7885102a0ea28dde6ad4e65b64c4bdff
Marcin Kaluza <marcin.kaluza@trioptimum.com>
2015-01-11 15:47:05 +0000

    merge: 66eb9fa -- drivers/net/wireless/bcmdhd/include/bcmcdc.h

--- drivers/net/wireless/bcmdhd/include/bcmcdc.h
+++ drivers/net/wireless/bcmdhd/include/bcmcdc.h
@@ -4,9 +4,15 @@
  *
  * Definitions subject to change without notice.
  *
+<<<<<<< ours
  * Copyright (C) 1999-2012, Broadcom Corporation
  * 
  *      Unless you and Broadcom execute a separate written software license
+=======
+ * Copyright (C) 1999-2011, Broadcom Corporation
+ * 
+ *         Unless you and Broadcom execute a separate written software license
+>>>>>>> theirs
  * agreement governing use of this software, this software is licensed to you
  * under the terms of the GNU General Public License version 2 (the "GPL"),
  * available at http://www.broadcom.com/licenses/GPLv2.php, with the
